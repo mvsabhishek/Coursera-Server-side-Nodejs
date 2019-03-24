9c68531d71fc5da23765a8111dd84f6a4f19a0c6
mvsabhishek <mvsabhishek@gmail.com>
2019-03-23 17:40:25 -0700

    Basic Authentication

--- app.js
+++ app.js
@@ -32,8 +32,34 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+function auth(req, res, next) {
+    console.log(req.headers);
+
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+        var err = new Error('You are not authenticated.')
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        return next(err);
+    }
+    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var username = auth[0];
+    var password = auth[1];
+
+    if (username === 'admin' && password === 'password') {
+        next();
+    } else {
+        var err = new Error('You are not authenticated.')
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        return next(err);
+    }
+}
+
+app.use(auth);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Configure routes
 app.use('/', indexRouter);
